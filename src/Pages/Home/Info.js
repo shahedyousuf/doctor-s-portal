3adb9cdc9b2b47d6d399cfd3b8c17002b510f302
shahedyousuf <smshahed.yousuf@gmail.com>
2022-05-10 07:49:29 +0200

    navbar added

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+
+const Info = () => {
+    return (
+        <div>
+            <InfoCard></InfoCard>
+        </div>
+    );
+};
+
+export default Info;
